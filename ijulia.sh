098587e1912661905226f4a1e6a9e51c23e59966
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-02-22 10:40:43 -0500

    Update ijulia.sh

--- ijulia.sh
+++ ijulia.sh
@@ -13,6 +13,7 @@ if [ -f /etc/secured ]; then
         exec jupyter notebook
 elif [ -f /etc/msg_secured ]; then
          #do nothing
+         wait 1s
 else
          echo 'To be able to used it need to secured by running: docker exec -it container_id after_install '
          echo 'To be able to used it need to secured by running: docker exec -it container_id after_install ' >> /var/log/ijulia.log 2>&1
